89fbbbd9536cb5d80b9e7bce30665eb26ced0af6
Liang Sun <sunly917@gmail.com>
2023-08-27 01:20:22 +0300

    fix: styling updates

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,27 @@ import { Box, Container, Heading, Text, SimpleGrid } from '@chakra-ui/react'
 
 export default function Home() {
     return (
-        <Container pt="8" height={'100vh'}>
-            <SimpleGrid height={'100%'} placeItems={'center'}>
+        <Container maxW={'6xl'} textAlign={'center'}>
+            <SimpleGrid height={'100vh'} placeItems={'center'}>
                 <Box>
-                    <Heading mb="4">Hi there 👋</Heading>
+                    <Heading as="h1" mb="6" size={{ base: 'md', lg: '3xl' }}>
+                        Hi there 👋
+                    </Heading>
 
-                    <Text mb="2">👨🏻‍💻 I am a web developer.</Text>
-                    <Text mb="2">📍 Located in Espoo, Finland.</Text>
-                    <Text mb="2">
+                    <Text fontSize={{ base: 'md', lg: '3xl' }} mb="2">
+                        👨🏻‍💻 I am a web developer.
+                    </Text>
+                    <Text fontSize={{ base: 'md', lg: '3xl' }} mb="2">
+                        📍 Located in Espoo, Finland.
+                    </Text>
+                    <Text fontSize={{ base: 'md', lg: '3xl' }} mb="2">
                         ✌️ Developing both in frontend and backend.
                     </Text>
-                    <Text mb="2">
+                    <Text fontSize={{ base: 'md', lg: '3xl' }} mb="2">
                         🚀 Preferable with PHP, Golang, Ruby, Python and
                         Javascript.
                     </Text>
-                    <Text mb="2">
+                    <Text fontSize={{ base: 'md', lg: '3xl' }} mb="2">
                         ❤️ Love chess, reading, fishing, photographing, and many
                         sports.
                     </Text>

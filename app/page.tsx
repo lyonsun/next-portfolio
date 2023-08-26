3a05aaa3feea4ec7e50f8da907027e2e8e457960
Liang Sun <sunly917@gmail.com>
2023-08-27 00:56:30 +0300

    build: use chakra ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,29 @@
+'use client'
+
+import { Box, Container, Heading, Text, SimpleGrid } from '@chakra-ui/react'
+
 export default function Home() {
-    return <div>Hello world</div>
+    return (
+        <Container pt="8" height={'100vh'}>
+            <SimpleGrid height={'100%'} placeItems={'center'}>
+                <Box>
+                    <Heading mb="4">Hi there 👋</Heading>
+
+                    <Text mb="2">👨🏻‍💻 I am a web developer.</Text>
+                    <Text mb="2">📍 Located in Espoo, Finland.</Text>
+                    <Text mb="2">
+                        ✌️ Developing both in frontend and backend.
+                    </Text>
+                    <Text mb="2">
+                        🚀 Preferable with PHP, Golang, Ruby, Python and
+                        Javascript.
+                    </Text>
+                    <Text mb="2">
+                        ❤️ Love chess, reading, fishing, photographing, and many
+                        sports.
+                    </Text>
+                </Box>
+            </SimpleGrid>
+        </Container>
+    )
 }

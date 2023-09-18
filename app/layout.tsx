17bcaefc5453e4e7e593cdd27c69de70c10023fe
Liang Sun <sunly917@gmail.com>
2023-09-18 21:32:55 +0300

    feat: add gtm scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Roboto_Mono } from 'next/font/google';
 import { Providers } from './providers';
+import Script from 'next/script';
 
 const robotoMono = Roboto_Mono({ subsets: ['latin'] });
 
@@ -17,6 +18,27 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
+            {process.env.NODE_ENV === 'production' && (
+                <>
+                    <Script
+                        async
+                        src="https://www.googletagmanager.com/gtag/js?id=G-ZGT4BC0C4P"
+                    />
+                    <Script
+                        id="google-tag-namager"
+                        dangerouslySetInnerHTML={{
+                            __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-ZGT4BC0C4P');
+                `,
+                        }}
+                    ></Script>
+                </>
+            )}
+
             <body className={robotoMono.className}>
                 <Providers>{children}</Providers>
             </body>

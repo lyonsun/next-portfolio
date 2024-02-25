9c650b4b3143b6730d42ee4d7b4c95a70e9c53e4
Liang Sun <sunly917@gmail.com>
2024-02-25 23:56:56 +0200

    feat: use callback and special treatment for first revealing

--- app/components/Headline.tsx
+++ app/components/Headline.tsx
@@ -1,38 +1,46 @@
 'use client';
 
-import { FC, useEffect, useState } from 'react';
+import { FC, useCallback, useEffect, useState } from 'react';
 import { chars, defaultHeadline, headlines } from '../data';
 
 const Headline: FC = () => {
     const [headline, setHeadline] = useState(defaultHeadline);
+    const [interalId, setInteralId] = useState(0);
 
-    let interalId = 0;
-    let numberOfIteration = 0;
-    const updateHeadline = (target: HTMLSpanElement) => {
-        if (interalId) {
-            window.clearInterval(interalId);
-        }
-        const newHeadline =
-            headlines[
-                Math.floor(Math.random() * headlines.length)
-            ].toUpperCase();
-        setHeadline(newHeadline);
-        interalId = window.setInterval(() => {
-            target.innerText = newHeadline
-                .split('')
-                .map((char, index) => {
-                    return index < numberOfIteration
-                        ? char
-                        : chars[Math.floor(Math.random() * chars.length)];
-                })
-                .join('');
-            if (numberOfIteration > newHeadline.length) {
+    const updateHeadline = useCallback(
+        (target: HTMLSpanElement) => {
+            if (interalId) {
                 window.clearInterval(interalId);
             }
+            const newHeadline =
+                headlines[
+                    Math.floor(Math.random() * headlines.length)
+                ].toUpperCase();
+            setHeadline(newHeadline);
 
-            numberOfIteration += 1;
-        }, 100);
-    };
+            let numberOfIteration = 0;
+            const newInteralId = window.setInterval(() => {
+                target.innerText = newHeadline
+                    .split('')
+                    .map((char, index) => {
+                        return index < numberOfIteration
+                            ? char
+                            : chars[
+                                  Math.floor(Math.random() * chars.length)
+                              ].toUpperCase();
+                    })
+                    .join('');
+                if (numberOfIteration > newHeadline.length) {
+                    window.clearInterval(interalId);
+                }
+
+                numberOfIteration++;
+            }, 100);
+
+            setInteralId(newInteralId);
+        },
+        [interalId]
+    );
 
     useEffect(() => {
         const mainIntervalId = window.setInterval(() => {
@@ -42,7 +50,18 @@ const Headline: FC = () => {
         }, 4000);
 
         return () => window.clearInterval(mainIntervalId);
-    });
+    }, [updateHeadline]);
+
+    useEffect(() => {
+        const timeoutId = window.setTimeout(() => {
+            updateHeadline(
+                document.querySelector('.headline') as HTMLHeadingElement
+            );
+        }, 1000);
+
+        return () => window.clearTimeout(timeoutId);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return (
         <div className="relative">
